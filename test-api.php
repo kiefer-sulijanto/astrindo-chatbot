d7fbae5242f66fe3a71ce24410b44abd11804163
Kiefer Sulijanto <kiefer.sulijanto@gmail.com>
2025-08-28 00:43:48 +0700

    Clean commit: removed secrets, added .gitignore

--- test-api.php
+++ test-api.php
@@ -3,7 +3,7 @@
 $ch = curl_init("https://api.openai.com/v1/models");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Authorization: Bearer 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 "
+    "Authorization: Bearer " // <-- Ganti dengan OpenAI API KEY kamu"
 ]);
 $response = curl_exec($ch);
 if (curl_errno($ch)) {
